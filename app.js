b66c49fad6e31c48062538b778fb2d6ec4c21695
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-03-28 19:25:11 -0700

    users can create events

--- app.js
+++ app.js
@@ -6,23 +6,45 @@ const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
+const bodyParser = require('body-parser');
+const models = require('./db/models');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 // Use "main" as our default layout
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
-// INDEX
-app.get('/events', (req, res) => {
-  res.render('events-index', { events: events });
+// Index
+app.get('/', (req, res) => {
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+    res.render('events-index', { events: events });
+  })
 })
+
+// NEW
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+})
+
+// CREATE
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+
 // OUR MOCK ARRAY OF PROJECTS
 var events = [
   { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
   { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
-
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
---
+++ db/models/event.js
@@ -0,0 +1,13 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const Event = sequelize.define('Event', {
+    title: DataTypes.STRING,
+    desc: DataTypes.TEXT,
+    imgUrl: DataTypes.TEXT //add this line (don't forget the comma above!)
+  }, {});
+  return Event;
+};
