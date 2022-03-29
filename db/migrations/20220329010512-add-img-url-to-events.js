275177f32a6a481a6d5e80c82abf321a090987cc
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-03-29 16:38:13 -0700

    commit

--- db/migrations/20220329010512-add-img-url-to-events.js
+++ db/migrations/20220329010512-add-img-url-to-events.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Events', 'imgUrl', { type: Sequelize.TEXT });
+    return queryInterface.addColumn('Events', 'imgUrl', { type: DataTypes.Text });
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Events', 'imgUrl');
   }
-};
+}; 
